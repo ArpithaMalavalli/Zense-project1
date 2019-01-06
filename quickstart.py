557fecac858bd393034d8d6e5c2611da1e6075fa
DREAMERS3 <arpitha.srivathsa@gmail.com>
2019-01-06 23:29:44 +0530

    added timetable and addition of event opotion

--- quickstart.py
+++ quickstart.py
@@ -4,6 +4,8 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import datetime
+from datetime import date
+import calendar
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar'
@@ -41,39 +43,59 @@ def check():
 # https://developers.google.com/calendar/quickstart/python
 # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
 # stored credentials.
-def add():
-	date=datetime.datetime.now()
-	str1='2019-'+str(format(date.month,'02d'))+'-06T10:00:00+05:30'
-	event = {
-	  'summary': 'Google I/O 2015',
-	  'location': '800 Howard St., San Francisco, CA 94103',
-	  'description': 'A chance to hear more about Google\'s developer products.',
-	  'start': {
-	    'dateTime': '2019-01-06T09:00:00+05:30',
-	    'timeZone':'Asia/Kolkata',
-	  },
+
+class Weekday:
+	def __init__(self,event_list,stime_list,etime_list):
+		self.eventl=event_list
+		self.startl=stime_list
+		self.endl=etime_list
+
+Sunday=Weekday(['MAD'],['09:00:00'],['12:00:00'])
+Monday=Weekday(['Computer Network','Digital Design','DSA lab'],['09:15:00','11:00:00','13:30:00'],['10:45:00','12:30:00','15:30:00'])
+Tuesday=Weekday(['DSA','Math-2','PE'],['09:15:00','11:00:00','16:00:00'],['10:45:00','12:30:00','17:30:00'])
+Wednesday=Weekday(['Computer Network','Technical communication'],['09:15:00','13:30:00'],['10:45:00','15:30:00'])
+Thursday=Weekday(['DSA','Digital Design','DSA tutorial','Comp networks tutorial'],['09:15:00','11:00:00','13:30:00','14:30:00'],['10:45:00','12:30:00','14:30:00','15:30:00'])
+Friday=Weekday(['Math-2','computational chemistry','Digital design tutorial','Math tutorial'],['09:15:00','11:00:00','13:30:00','14:30:00'],['10:45:00','12:30:00','14:30:00','15:30:00'])
+Saturday=Weekday([],[],[])
+
+day_to_obj={'Sunday':Sunday}	
+
+
+def add(wday):
+	print (wday.eventl)
+	for i in range(len(wday.eventl)): 
+		event = {
+		  'summary': wday.eventl[i],
+		 # 'location': '800 Howard St., San Francisco, CA 94103',
+		 # 'description': 'A chance to hear more about Google\'s developer products.',
+		  'start': {
+		    'dateTime': '2019-01-06T'+wday.startl[i]+'+05:30',
+		    'timeZone':'Asia/Kolkata',
+		  },
+		
+		  'end': {
+		    'dateTime': '2019-01-06T'+wday.endl[i]+'+05:30',
+		    'timeZone': 'Asia/Kolkata',
+		  },
+		 # 'recurrence': [
+		 #   "RRULE:FREQ=WEEKLY;UNTIL=20191030T065959Z"
+		 # ],
+		  'reminders': {
+		    'useDefault': False,
+		    'overrides': [
+		      {'method': 'email', 'minutes': 24 * 60},
+		      {'method': 'popup', 'minutes': 10},
+		    ],
+		  },
+		}
+
+		event = service.events().insert(calendarId='primary', body=event).execute()
+	print ('Events for the day created:')#,(event.get('htmlLink')))
+
+
 	
-	  'end': {
-	    'dateTime': str1,
-	    'timeZone': 'Asia/Kolkata',
-	  },
-	  'recurrence': [
-	    "RRULE:FREQ=WEEKLY;UNTIL=20191030T065959Z"
-	  ],
-	  'reminders': {
-	    'useDefault': False,
-	    'overrides': [
-	      {'method': 'email', 'minutes': 24 * 60},
-	      {'method': 'popup', 'minutes': 10},
-	    ],
-	  },
-	}
-
-	event = service.events().insert(calendarId='primary', body=event).execute()
-	print ('Event created:',(event.get('htmlLink')))
-
-
 if __name__ == '__main__':
-    check()
-    add()
+	check()
+	my_date = date.today() 
+	add(day_to_obj[calendar.day_name[my_date.weekday()]])
     
